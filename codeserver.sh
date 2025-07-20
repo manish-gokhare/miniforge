a9c08858a1fd6e4ed6c8d262f35a0049726b7bdb
manish-gokhare <59787265+manish-gokhare@users.noreply.github.com>
2025-07-21 00:28:25 +0200

    Update codeserver.sh

--- codeserver.sh
+++ codeserver.sh
@@ -9,3 +9,7 @@ conda activate myenvs
 # Install code-server using npm
 npm install -g code-server
 
+code-server --auth none --port 8888 --bind-addr 0.0.0.0:8888 --disable-telemetry
+
+<externalIP>:8888
+
