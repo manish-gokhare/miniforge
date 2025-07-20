d28f550783ccfd2b582b693bacba25e40d4262be
manish-gokhare <59787265+manish-gokhare@users.noreply.github.com>
2025-07-20 23:24:34 +0200

    Update 3_generate_jupyter_config.py

--- 3_generate_jupyter_config.py
+++ 3_generate_jupyter_config.py
@@ -6,5 +6,17 @@ mkdir -p /etc/jupyterhub
 cd /etc/jupyterhub
 
 # Generate default config
-/opt/miniforge/envs/myenvs/bin/jupyterhub --generate-config
+/opt/miniforge/envs/myenvs/bin/jupyterhub --generate-config 
+
+
+
+#Add below in /etc/jupyterhub/
+
+sudo vi /etc/jupyterhub/jupyterhub_config.py
+
+c.JupyterHub.bind_url = 'http://:8000'  # Or set to your IP:port
+c.Authenticator.class_ = 'jupyterhub.auth.PAMAuthenticator'
+c.Spawner.default_url = '/lab'  # Optional: default to JupyterLab if installed
+c.Spawner.cmd = ['/opt/miniforge/envs/myenvs/bin/jupyter-labhub']
+
 
